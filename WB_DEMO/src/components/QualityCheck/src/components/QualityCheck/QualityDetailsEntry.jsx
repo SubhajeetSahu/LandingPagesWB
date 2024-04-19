df6e73717242269c88eea15facbdcaca9a5e6e5a
giridharpatnaik183 <giridharpatnaik183@gmail.com>
2024-04-19 18:24:44 +0530

    new changes made

--- WB_DEMO/src/components/QualityCheck/src/components/QualityCheck/QualityDetailsEntry.jsx
+++ WB_DEMO/src/components/QualityCheck/src/components/QualityCheck/QualityDetailsEntry.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { Chart, ArcElement } from "chart.js/auto";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRectangleXmark } from "@fortawesome/free-solid-svg-icons";
+import { faSave, faTrashAlt, faPrint, faTimes } from "@fortawesome/free-solid-svg-icons";
 import SideBar3 from "../../../../SideBar/SideBar3";
 import Header from "../../../../Header/Header";
 import "./QualityDetailsEntry.css";
@@ -14,26 +14,26 @@ const QualityDetailsEntry = () => {
     navigate("/QualityCheck");
   };
 
-    const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
-    const isTablet = useMediaQuery({ query: '(min-width: 768px) and (max-width: 1023px)' });
-    const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const isTablet = useMediaQuery({ query: '(min-width: 768px) and (max-width: 1023px)' });
+  const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' });
 
   const img = "https://example.com/your-image.jpg";
-  <img src="example.jpg" alt="Example" loading="lazy" />
+  <img src="example.jpg" alt="Example" loading="lazy" />;
   const currentDateTime = new Date().toLocaleString();
 
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
   const chartRef = useRef(null);
   const chartRef2 = useRef(null);
   const homeMainContentRef = useRef(null);
- 
+
   const toggleSidebar = () => {
     setIsSidebarExpanded(!isSidebarExpanded);
   };
- 
+
   useEffect(() => {
     Chart.register(ArcElement);
- 
+
     const resizeObserver = new ResizeObserver(() => {
       if (
         homeMainContentRef.current &&
@@ -44,11 +44,11 @@ const QualityDetailsEntry = () => {
         chartRef2.current.chartInstance.resize();
       }
     });
- 
+
     if (homeMainContentRef.current) {
       resizeObserver.observe(homeMainContentRef.current);
     }
- 
+
     return () => {
       resizeObserver.disconnect();
     };
@@ -56,54 +56,66 @@ const QualityDetailsEntry = () => {
 
   return (
     <div className="d-flex">
-
       <div className="flex-grow-1">
-        {/* Add the header */}
         <Header toggleSidebar={toggleSidebar} />
 
-    <SideBar3
-      isSidebarExpanded={isSidebarExpanded}
-      toggleSidebar={toggleSidebar}
-    />
+        <SideBar3
+          isSidebarExpanded={isSidebarExpanded}
+          toggleSidebar={toggleSidebar}
+        />
 
-<div className={`qualitydeatilcheckmain-content ${isMobile ? 'mobile' : isTablet ? 'tablet' : 'desktop'}`}>
-          <div
-            className="container-fluid trans_form_main_div overflow-hidden"
-            style={{ marginTop: "50px",  marginLeft: "20px"}}
-          >
+        <div className={`quality-detail-check-main-content ${isMobile ? 'mobile' : isTablet ? 'tablet' : 'desktop'}`}>
+          <div className="container-fluid trans-form-main-div overflow-hidden">
             <div className="close" onClick={closeForm}>
-              <FontAwesomeIcon icon={faRectangleXmark} />
+              <FontAwesomeIcon icon={faTimes} />
             </div>
 
-          <h1 className="text-center mb-4"> Transaction Form </h1>
-          <div className="row">
-            <div className="col-lg-4 " style={{ marginLeft: "50px" }}>
-              <div className="d-flex mb-3">
-                <label htmlFor="userId" className="form-label text1 me-2">
-                  Date:
-                </label>
-                <input
-                  type="date"
-                  autoComplete="off"
-                  required
-                  className="form-control"
-                />
-              </div>
-              <div className="d-flex mb-3">
-                <label htmlFor="userId" className="form-label text1 me-2">
-                  In Time:
-                </label>
-                <input
-                  type="text"
-                  autoComplete="off"
-                  value={currentDateTime}
-                  required
-                  className="form-control"
-                />
-              </div>
+            <div className="d-flex justify-content-between align-items-center mb-4">
+            <div style={{ textAlign: "center" }}>
+  <h2 className="mb-4" style={{ display: "inline-block", marginLeft: "500px" }}>Transaction Form</h2>
+</div>
 
-              <div className="div2">
-                <div className="d-flex mb-1">
+
+
+              <div className="buttons-container">
+                <button className="btn btn-primary button-transition">
+                  <FontAwesomeIcon icon={faSave} />
+                </button>
+                <button className="btn btn-danger button-transition">
+                  <FontAwesomeIcon icon={faTrashAlt} />
+                </button>
+                <button className="btn btn-secondary button-transition">
+                  <FontAwesomeIcon icon={faPrint} />
+                </button>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-4" style={{ marginLeft: "50px" }}>
+                <div className="d-flex mb-3">
+                  <label htmlFor="userId" className="form-label text1 me-2">
+                    Date:
+                  </label>
+                  <input
+                    type="date"
+                    autoComplete="off"
+                    required
+                    className="form-control"
+                  />
+                </div>
+                <div className="d-flex mb-3">
+                  <label htmlFor="userId" className="form-label text1 me-2">
+                    In Time:
+                  </label>
+                  <input
+                    type="text"
+                    autoComplete="off"
+                    value={currentDateTime}
+                    required
+                    className="form-control"
+                  />
+                </div>
+                <div className="d-flex mb-3">
                   <label htmlFor="userId" className="form-label text1 me-2">
                     Customer:
                   </label>
@@ -115,8 +127,7 @@ const QualityDetailsEntry = () => {
                     className="form-control"
                   />
                 </div>
-
-                <div className="d-flex mb-1">
+                <div className="d-flex mb-3">
                   <label htmlFor="userId" className="form-label text1 me-2">
                     Supplier:
                   </label>
@@ -128,7 +139,6 @@ const QualityDetailsEntry = () => {
                     className="form-control"
                   />
                 </div>
-
                 <div className="d-flex mb-1">
                   <label htmlFor="userId" className="form-label text1 me-2">
                     Truck Number:
@@ -168,7 +178,65 @@ const QualityDetailsEntry = () => {
                   />
                 </div>
 
-                <div className="d-flex mb-1">
+                
+              </div>
+              <div className="col-lg-4 div2 container-fluid">
+              <div className="d-flex mb-3">
+                  <label htmlFor="userId" className="form-label text1 me-2">
+                    Ticket No:
+                  </label>
+                  <input
+                    type="text"
+                    autoComplete="off"
+                    required
+                    className="form-control"
+                  />
+                </div>
+                <div className="d-flex mb-3">
+                  <label htmlFor="userId" className="form-label text1 me-2">
+                    Po No:
+                  </label>
+                  <input
+                    type="text"
+                    autoComplete="off"
+                    required
+                    className="form-control"
+                  />
+                </div>
+                <div className="d-flex mb-3">
+                <label htmlFor="userId" className="form-label text1 me-2">
+                  Challan No:
+                </label>
+                <input
+                  type="text"
+                  autoComplete="off"
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div className="d-flex mb-3">
+                <label htmlFor="userId" className="form-label text1 me-2">
+                  Status:
+                </label>
+                <input
+                  type="text"
+                  autoComplete="off"
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div className="d-flex mb-3">
+                <label htmlFor="userId" className="form-label text1 me-2">
+                  Remark:
+                </label>
+                <input
+                  type="text"
+                  autoComplete="off"
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div className="d-flex mb-1">
                   <label htmlFor="userId" className="form-label text1 me-2">
                     Product:
                   </label>
@@ -206,143 +274,9 @@ const QualityDetailsEntry = () => {
                 </div>
               </div>
             </div>
-            <div className="col-lg-4 div3 container-fluid">
-              <div className="d-flex mb-3">
-                <label htmlFor="userId" className="form-label text1 me-2">
-                  Ticket No:
-                </label>
-                <input
-                  type="text"
-                  autoComplete="off"
-                  required
-                  className="form-control"
-                />
-              </div>
-              <div className="d-flex mb-3">
-                <label htmlFor="userId" className="form-label text1 me-2">
-                  Po No:
-                </label>
-                <input
-                  type="text"
-                  autoComplete="off"
-                  required
-                  className="form-control"
-                />
-              </div>
-              <div className="d-flex mb-3">
-                <label htmlFor="userId" className="form-label text1 me-2">
-                  Challan No:
-                </label>
-                <input
-                  type="text"
-                  autoComplete="off"
-                  required
-                  className="form-control"
-                />
-              </div>
-              <div className="d-flex mb-3">
-                <label htmlFor="userId" className="form-label text1 me-2">
-                  Status:
-                </label>
-                <input
-                  type="text"
-                  autoComplete="off"
-                  required
-                  className="form-control"
-                />
-              </div>
-              <div className="d-flex mb-3">
-                <label htmlFor="userId" className="form-label text1 me-2">
-                  Remark:
-                </label>
-                <input
-                  type="text"
-                  autoComplete="off"
-                  required
-                  className="form-control"
-                />
-              </div>
-              <div className="text-center img">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th colSpan="5">
-                        <b>Camera</b>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td colSpan="2" rowSpan="2">
-                        <img
-                          src={img}
-                          alt="img" width={150} height={200}
-                          className="img-transition"
-                        />
-                         <button onClick={() => captureImage(index)} className="btn btn-primary mt-2">Capture</button>
-                      </td>
-                      <td colSpan="2" rowSpan="2">
-                        <img
-                          src={img}
-                          alt="img"
-                          width={150}
-                          height={200}
-                          className="img-transition"
-                        />
-                         <button onClick={() => captureImage(index)} className="btn btn-primary mt-2">Capture</button>
-                      </td>
-                    </tr>
-                    <tr></tr>
-                    <tr>
-                      <td colSpan="2" rowSpan="2">
-                        <img
-                          src={img}
-                          alt="img"
-                          width={150}
-                          height={200}
-                          className="img-transition"
-                        />
-                        <button onClick={() => captureImage(index)} className="btn btn-primary mt-2">Capture</button>
-                      </td>
-                      <td colSpan="2" rowSpan="2">
-                        <img
-                          src={img}
-                          alt="img"
-                          width={150}
-                          height={200}
-                          className="img-transition"
-                        />
-                         <button onClick={() => captureImage(index)} className="btn btn-primary mt-2">Capture</button>
-                      </td>
-                    </tr>
-                    <tr></tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-            <div className="col-lg-2 container-fluid">
-              <div className="right_div">
-                <button className="btn btn-primary mb-2 button-transition">
-                  Save[F10]
-                </button>
-                <button className="btn btn-danger mb-2 button-transition">
-                  Delete[F9]
-                </button>
-                <button className="btn btn-secondary button-transition">
-                  Print
-                </button>
-                <button className="btn btn-success button-transition">
-                  Approve
-                </button>
-                <button className="btn btn-warning button-transition">
-                  Reject
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
-     </div>
     </div>
   );
 };
