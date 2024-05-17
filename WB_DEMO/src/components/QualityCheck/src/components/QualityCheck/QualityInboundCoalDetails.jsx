0ec90f578575e6dc4f5f11aa6607f2ed85f24e3c
ldtech-weighbridge <ashutosh.samantaray@ldtech.in>
2024-05-17 18:03:12 +0530

    quality new api added for inbound and outbound

--- WB_DEMO/src/components/QualityCheck/src/components/QualityCheck/QualityInboundCoalDetails.jsx
+++ WB_DEMO/src/components/QualityCheck/src/components/QualityCheck/QualityInboundCoalDetails.jsx
@@ -5,14 +5,25 @@ import {
   faSave,
   faEraser,
   faPrint,
-  faEraser,
 } from "@fortawesome/free-solid-svg-icons";
 import SideBar3 from "../../../../SideBar/SideBar3";
 import { useMediaQuery } from "react-responsive";
 import { Chart, ArcElement } from "chart.js/auto";
+import styled from 'styled-components';
 
 // Import Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const MainContent = styled.div`
+  min-height: calc(100vh - 80vh); /* Adjust the value (56px) as needed for your navbar height */
+  display: flex;
+  flex-direction: column;
+`;
+
+const TransFormMainDiv = styled.div`
+  flex-grow: 1;
+  overflow-y: auto;
+`;
 
 const QualityInboundCoalDetails = () => {
   const navigate = useNavigate();
@@ -20,8 +31,8 @@ const QualityInboundCoalDetails = () => {
 
   const [formData, setFormData] = useState({
     date: null,
-    inTime: null,
-    outTime: null,
+    in: null, // Changed from inTime
+    out: null, // Changed from outTime
     vehicleNo: null,
     transporterName: null,
     transactionType: null,
@@ -35,7 +46,7 @@ const QualityInboundCoalDetails = () => {
     materialType: null,
   });
 
-  const [parameters, setparameters] = useState({});
+  const [parameters, setParameters] = useState({});
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -55,14 +66,21 @@ const QualityInboundCoalDetails = () => {
       materialName: urlParams.get("materialName"),
       materialType: urlParams.get("materialType"),
     };
-
+  
     setFormData(urlData);
-
-    const fetchparameters = async () => {
+  
+    const fetchParameters = async () => {
       try {
-        const response = await fetch(
-          `http://localhost:8080/api/v1/materials/${urlData.materialName}/parameters`
-        );
+        let response;
+        if (urlData.transactionType === "Inbound") {
+          response = await fetch(
+            `http://localhost:8080/api/v1/materials/parameters?materialName=${urlData.materialName}&supplierName=${urlData.supplierOrCustomerName}&supplierAddress=${urlData.supplierOrCustomerAddress}`
+          );
+        } else {
+          response = await fetch(
+            `http://localhost:8080/api/v1/products/parameters?productName=${urlData.materialName}&customerName=${urlData.supplierOrCustomerName}&customerAddress=${urlData.supplierOrCustomerAddress}`
+          );
+        }
         const data = await response.json();
         if (data.length > 0 && data[0].parameters) {
           const ranges = data[0].parameters.reduce((acc, parameter) => {
@@ -72,17 +90,18 @@ const QualityInboundCoalDetails = () => {
             };
             return acc;
           }, {});
-          setparameters(ranges);
+          setParameters(ranges);
         }
       } catch (error) {
         console.error("Error fetching parameter ranges:", error);
       }
     };
-
+  
     if (urlData.materialName && urlData.materialType) {
-      fetchparameters();
+      fetchParameters();
     }
   }, []);
+  
 
   const handleSave = async () => {
     const data = {
@@ -98,11 +117,10 @@ const QualityInboundCoalDetails = () => {
       supplierOrCustomerAddress: formData.supplierOrCustomerAddress,
       materialName: formData.materialName,
       materialType: formData.materialType,
-      moisture: formData.moisture || 0,
-      vm: formData.vm || 0,
-      ash: formData.ash || 0,
-      fc: formData.fc || 0,
+      ...formData,
     };
+
+    console.log("Form data being sent:", data); // Added console log
 
     try {
       const response = await fetch(
@@ -113,7 +131,7 @@ const QualityInboundCoalDetails = () => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
-          credentials: "include"
+          credentials: "include",
         }
       );
 
@@ -149,10 +167,13 @@ const QualityInboundCoalDetails = () => {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value === "" ? null : value,
+    }));
   };
-
   
+
   useEffect(() => {
     // Any side effect code can be placed here
     // console.log("Updated state:", formData);
@@ -162,9 +183,8 @@ const QualityInboundCoalDetails = () => {
     const parameter = parameters[parameterName];
     if (!parameter) return `${parameterName}`;
     const { rangeFrom, rangeTo } = parameter;
-    return `${parameterName} % (${rangeFrom}-${rangeTo})`;
+    return `${parameterName} (${rangeFrom}-${rangeTo})`;
   };
-
 
   const handleKeyPress = (event) => {
     // Allow only numeric characters and certain special characters like dot (.) and minus (-)
@@ -173,106 +193,134 @@ const QualityInboundCoalDetails = () => {
       event.preventDefault();
     }
   };
-  
-  const renderFieldWithBox = (fieldName, fieldValue, propertyName, onChange, isReadOnly = false, isRequired = false) => {
-    const inputStyle = isReadOnly ? { borderColor: '#ced4da', backgroundColor: 'rgb(239, 239, 239)' } : {};
-    const asteriskStyle = isRequired ? { color: 'red' } : {};
-  
-    return (
-      <div className="col-md-3 mb-3">
-        <label htmlFor={propertyName} className="form-label">
-          {fieldName}:
-          {isRequired && <span style={asteriskStyle}>*</span>}
-        </label>
-        <input
-          type="text"
-          name={propertyName}
-          autoComplete="off"
-          value={fieldValue === null ? "" : fieldValue}
-          onChange={onChange}
-          onKeyPress={handleKeyPress} // Attach onKeyPress event handler
-          required={isRequired}
-          readOnly={isReadOnly}
-          style={inputStyle}
-          className="form-control"
-          id={propertyName}
-        />
-      </div>
-    );
-  };
-  
 
-  const handleClear = () => {
-    setFormData({
-      ...formData,
-      moisture: "",
-      vm: "",
-      ash: "",
-      fc: "",
-    });
-  };
-  
-  
-
-
-
+ const renderFieldWithBox = (
+  fieldName,
+  propertyName,
+  isReadOnly = false,
+  isRequired = false
+) => {
+  const inputStyle = isReadOnly
+    ? { borderColor: "#ced4da", backgroundColor: "rgb(239, 239, 239)" }
+    : {};
+  const asteriskStyle = isRequired ? { color: "red" } : {};
+  const value = formData[propertyName] !== null ? formData[propertyName] : "-";
 
   return (
-    <SideBar3>
-      <div className="d-flex">
-        <div className="flex-grow-1">
-          <h2 className="text-center p-2" style={{ fontFamily: "Arial", fontSize: "clamp(12px, 4vw, 30px)" }}>Ticket Insights & Quality Inputs</h2>
-          <div className="main-content mt-0 ms-md-2">
-            <div className="container-fluid trans-form-main-div overflow-hidden">
-              <div className="d-flex justify-content-between align-items-center mb-2 ml-6">
-                <div></div> {/* This empty div will push the icons to the right */}
-                <div>
-                  <FontAwesomeIcon icon={faSave} className="btn-icon mx-3" onClick={handleSave} style={{ fontSize: '1.5em', color: '#008060' }} />
-                  <FontAwesomeIcon icon={faEraser} className="btn-icon mx-3" onClick={handleClear} style={{ fontSize: '1.5em', color: "#d63031" }} />
-                  <FontAwesomeIcon icon={faPrint} className="btn-icon mx-3" style={{ fontSize: '1.5em', color: '#3e8ee6' }} />
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="card mb-3 p-3 border shadow-lg">
-                    <div className="card-body">
-                      <div className="row">
-                        {renderFieldWithBox("TicketNo", formData.ticketNo, "ticketNo", handleInputChange, true)}
-                        {renderFieldWithBox("Date", formData.date, "date", handleInputChange, true)}
-                        {renderFieldWithBox("Vehicle Number", formData.vehicleNo, "vehicleNo", handleInputChange, true)}
-                        {renderFieldWithBox("Transporter", formData.transporterName, "transporterName", handleInputChange, true)}
-                        {renderFieldWithBox("Tp No", formData.tpNo, "tpNo", handleInputChange, true)}
-                        {renderFieldWithBox("Po No", formData.poNo, "poNo", handleInputChange, true)}
-                        {renderFieldWithBox("Challan No", formData.challanNo, "challanNo", handleInputChange, true)}
-                        {renderFieldWithBox("Material", formData.materialName, "materialName", handleInputChange, true)}
-                        {renderFieldWithBox("Material Type", formData.materialType, "materialType", handleInputChange, true)}
-                        {renderFieldWithBox("Supplier", formData.supplierOrCustomerName, "supplierOrCustomerName", handleInputChange, true)}
-                        {renderFieldWithBox("Supplier Address", formData.supplierOrCustomerAddress, "supplierOrCustomerAddress", handleInputChange, true)}
-                        {renderFieldWithBox("Transaction Type", formData.transactionType, "transactionType", handleInputChange, true)}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="card mb-3 p-3 border shadow-lg">
-                    <div className="card-body">
-                      <div className="row">
-                        {renderFieldWithBox(generateFieldNameWithRange("Moisture"), formData.moisture, "Moisture", handleInputChange, false, true)}
-                        {renderFieldWithBox(generateFieldNameWithRange("Vm"), formData.vm, "Vm", handleInputChange, false, true)}
-                        {renderFieldWithBox(generateFieldNameWithRange("Ash"), formData.ash, "Ash", handleInputChange, false, true)}
-                        {renderFieldWithBox(generateFieldNameWithRange("Fc"), formData.fc, "Fc", handleInputChange, false, true)}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </SideBar3>
-  );
+    <div className="col-md-3 mb-3">
+      <label htmlFor={propertyName} className="form-label">
+        {fieldName}:
+        {isRequired && <span style={asteriskStyle}>*</span>}
+        </label>
+     <input
+       type="text"
+       name={propertyName}
+       autoComplete="off"
+       value={value}
+       onChange={handleInputChange}
+       onKeyPress={handleKeyPress}
+       required={isRequired}
+       readOnly={isReadOnly}
+       style={inputStyle}
+       className="form-control"
+       id={propertyName}
+     />
+   </div>
+ );
+};
+
+ const handleClear = () => {
+   setFormData({
+     ...formData,
+     ...Object.keys(parameters).reduce((acc, parameterName) => {
+       acc[parameterName] = "";
+       return acc;
+     }, {}),
+   });
+ };
+ return (
+   <SideBar3>
+     <div className="d-flex">
+       <div className="flex-grow-1">
+         <h2
+           className="text-center p-2"
+           style={{ fontFamily: "Arial", fontSize: "clamp(12px, 4vw, 30px)" }}
+         >
+           Ticket Insights & Quality Inputs
+         </h2>
+         <MainContent>
+           <TransFormMainDiv>
+             <div className="container-fluid overflow-hidden">
+               <div className="d-flex justify-content-between align-items-center mb-2 ml-6">
+                 <div></div>
+                 <div>
+                   <FontAwesomeIcon
+                     icon={faSave}
+                     className="btn-icon mx-3"
+                     onClick={handleSave}
+                     style={{ fontSize: "1.5em", color: "#008060" }}
+                   />
+                   <FontAwesomeIcon
+                     icon={faEraser}
+                     className="btn-icon mx-3"
+                     onClick={handleClear}
+                     style={{ fontSize: "1.5em", color: "#d63031" }}
+                   />
+                   <FontAwesomeIcon
+                     icon={faPrint}
+                     className="btn-icon mx-3"
+                     style={{ fontSize: "1.5em", color: "#3e8ee6" }}
+                   />
+                 </div>
+               </div>
+               <div className="row">
+                 <div className="col-lg-12">
+                   <div className="card mb-3 p-2 border shadow-lg">
+                     <div className="card-body">
+                       <div className="row">
+                         {renderFieldWithBox("TicketNo", "ticketNo", true)}
+                         {renderFieldWithBox("Date", "date", true)}
+                         {renderFieldWithBox("Vehicle Number", "vehicleNo", true)}
+                         {renderFieldWithBox("Transporter", "transporterName", true)}
+                         {renderFieldWithBox("Tp No", "tpNo", true)}
+                         {renderFieldWithBox("Po No", "poNo", true)}
+                         {renderFieldWithBox("Challan No", "challanNo", true)}
+                         {renderFieldWithBox("Material", "materialName", true)}
+                         {renderFieldWithBox("Material Type", "materialType", true)}
+                         {renderFieldWithBox("Supplier/Customer", "supplierOrCustomerName", true)}
+                         {renderFieldWithBox("Supplier/Customer Address", "supplierOrCustomerAddress", true)}
+                         {renderFieldWithBox("Transaction Type", "transactionType", true)}
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+               <div className="row">
+                 <div className="col-lg-12">
+                   <div className="card mb-3 p-2 border shadow-lg">
+                     <div className="card-body">
+                       <div className="row">
+                         {Object.keys(parameters).map((parameterName) => (
+                           <React.Fragment key={parameterName}>
+                             {renderFieldWithBox(
+                               generateFieldNameWithRange(parameterName),
+                               parameterName,
+                               false,
+                               true
+                             )}
+                           </React.Fragment>
+                         ))}
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </TransFormMainDiv>
+         </MainContent>
+       </div>
+     </div>
+   </SideBar3>
+ );
 };
 export default QualityInboundCoalDetails;
